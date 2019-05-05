6fe6aaad3b95efd7aa495c8e0c2f6cc4433039d9
Marcos Paliari <marcos@paliari.com.br>
2019-05-04 23:22:36 -0300

    example

--- example/public/index.php
+++ example/public/index.php
@@ -7,12 +7,13 @@ header('X-Custom-Header: Value custom');
 <pre>
     <?
     $cert = $_SERVER['HTTP_X_SSL_CLIENT_CERT'];
-    $a = explode(' ', $cert);
-    echo "$cert\n";
-    echo "\n";
-    $cert = implode("\n", $a);
-    var_export($cert);
-    echo "\n";
+    $cert = str_replace(['-----BEGIN CERTIFICATE-----', '-----END CERTIFICATE-----', "\n"], '', $cert);
+    $cert = str_replace(["\r\n", "\n", "\r", "\t", ' '], '', $cert);
+    $cert = wordwrap($cert, 64, "\n", true);
+    $cert = rtrim($cert);
+    $cert = implode("\n", ['-----BEGIN CERTIFICATE-----', $cert, '-----END CERTIFICATE-----', '']);
+
+    echo "\n$cert\n";
     echo str_repeat('-', 100);
     echo "\n";
     var_export(apache_request_headers());
