5a9087fe31335d44f7996e9fef5e9b5fce005347
Marcos Paliari <marcos@paliari.com.br>
2019-05-05 09:48:46 -0300

    fix example

--- example/public/index.php
+++ example/public/index.php
@@ -5,21 +5,26 @@ header('X-Custom-Header: Value custom');
 <h1><?= $_ENV['APP_NAME']; ?></h1>
 <hr>
 <pre>
-    <?
-    $cert = $_SERVER['HTTP_X_SSL_CLIENT_CERT'];
-    $cert = str_replace(['-----BEGIN CERTIFICATE-----', '-----END CERTIFICATE-----', "\n"], '', $cert);
-    $cert = str_replace(["\r\n", "\n", "\r", "\t", ' '], '', $cert);
-    $cert = wordwrap($cert, 64, "\n", true);
-    $cert = rtrim($cert);
-    $cert = implode("\n", ['-----BEGIN CERTIFICATE-----', $cert, '-----END CERTIFICATE-----', '']);
+    <?php
+    if ($cert = $_SERVER['HTTP_X_SSL_CLIENT_CERT']) {
+        $begin = '-----BEGIN CERTIFICATE-----';
+        $end   = '-----END CERTIFICATE-----';
 
-    echo "\n$cert\n";
-    echo str_repeat('-', 100);
-    echo "\n";
+        $cert = str_replace([$begin, $end, "\n"], '', $cert);
+        $cert = str_replace(["\r\n", "\n", "\r", "\t", ' '], '', $cert);
+        $cert = wordwrap($cert, 64, "\n", true);
+        $cert = rtrim($cert);
+        $cert = implode("\n", [$begin, $cert, $end, '']);
+
+        echo "\n$cert\n";
+        echo str_repeat('-', 100);
+        echo "\n";
+    }
+    echo "HEADERS:\n";
     var_export(apache_request_headers());
     echo "\n";
     echo str_repeat('-', 100);
-    echo "\n";
+    echo "\nSERVER:\n";
     var_export($_SERVER);
     ?>
 </pre>
